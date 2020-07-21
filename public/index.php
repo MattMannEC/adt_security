73dfc95c442908e01f88ea482089fd8a885fb51c
mattmannec <mattmannec@gmail.com>
2020-07-21 11:47:31 +0200

    setup vhost

--- public/index.php
+++ public/index.php
@@ -2,27 +2,18 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
-// create dev
-
 switch ($request) {
     case '' :
         require __DIR__ . '/../views/index.php';
         break;
-    case $publicRoot . 'about' :
-        echo $publicRoot . 'about';
-        require __DIR__ . '/../views/about.php';
-        break;
-    case $publicRoot . 'about/' :
-        echo $publicRoot . 'about/';
-        require __DIR__ . '/../views/about.php';
-        break;
-    case $publicRoot . '/' :
-        echo $publicRoot . '/';
+    case '/' :
         require __DIR__ . '/../views/index.php';
         break;
-    case $publicRoot . '' :
-        echo $publicRoot . '' ;
-        require __DIR__ . '/../views/index.php';
+    case 'about' :
+        require __DIR__ . '/../views/about.php';
+        break;
+    case 'about/' :
+        require __DIR__ . '/../views/about.php';
         break;
     default:
         http_response_code(404);
