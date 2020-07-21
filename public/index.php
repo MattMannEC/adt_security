8537a6c66b343b2856fa6c4b7bed1db3e219c7b2
mattmannec <mattmannec@gmail.com>
2020-07-21 11:16:05 +0200

    set up

--- public/index.php
+++ public/index.php
@@ -2,27 +2,28 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
-$publicRoot = "/routing/";
-
 switch ($request) {
+    case '' :
+        require __DIR__ . '/../views/index.php';
+        break;
     case $publicRoot . 'about' :
         echo $publicRoot . 'about';
-        require __DIR__ . '/views/about.php';
+        require __DIR__ . '/../views/about.php';
         break;
     case $publicRoot . 'about/' :
         echo $publicRoot . 'about/';
-        require __DIR__ . '/views/about.php';
+        require __DIR__ . '/../views/about.php';
         break;
     case $publicRoot . '/' :
         echo $publicRoot . '/';
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/../views/index.php';
         break;
     case $publicRoot . '' :
         echo $publicRoot . '' ;
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/../views/index.php';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
+        require __DIR__ . '/../views/404.php';
         break;
 }
