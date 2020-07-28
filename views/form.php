20f385aab84e9a099b692912d7f50a0468b0ddca
mattmannec <mattmannec@gmail.com>
2020-07-28 11:42:55 +0200

    added required to html forms

--- views/form.php
+++ views/form.php
@@ -13,19 +13,19 @@ require 'header.php';
         <h2 class="formTitle py-2 px-1 py-sm-5">Get A Free Quote</h2>    
         <div class="form-group">
             <label for="exampleInputEmail1">Email address <span class="server-message email-message"></span></label>
-            <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Full Name <span class="server-message fullname-message"></span></label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="fullName" placeholder="Full Name">
+            <input type="text" class="form-control" id="exampleInputPassword1" name="fullName" placeholder="Full Name" required>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Post Code <span class="server-message postcode-message"></span></label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="postCode" maxlength="10" placeholder="Post Code">
+            <input type="text" class="form-control" id="exampleInputPassword1" name="postCode" maxlength="10" placeholder="Post Code" required>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Phone Number <span class="server-message phonenumber-message"></span></label>
-            <input type="text" class="form-control" id="exampleInputPassword1" name="phoneNumber" maxlength="12" placeholder="Phone Number">
+            <input type="text" class="form-control" id="exampleInputPassword1" name="phoneNumber" maxlength="12" placeholder="Phone Number" required>
         </div>
         
         <div id="server-results"></div>
